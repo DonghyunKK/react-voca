975a996bfa79d3448f6c56d53e9c8ba68d6857fc
donghyun kim <donghyunkim524@gmail.com>
2022-01-03 22:32:47 +0000

    add json-server and fetch data

--- src/component/DayList.js
+++ src/component/DayList.js
@@ -1,13 +1,25 @@
-import dummy from '../db/data.json';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function DayList(){
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/days")
+    .then(res => {
+      return res.json()
+    })
+    .then(data => {
+      setDays(data)
+    })
+  },[])
+
   return(
     <ul className='list_day'>
       {/* {dummy.days.map(day => (
         <li key={day.id}>Day {day.day}</li>
       ))} */}
-      {dummy.days.map((day) => {
+      {days.map((day) => {
         return <li key={day.id}>
           <Link to={`/day/${day.day}`}>Day  {day.day}</Link>
               </li>
