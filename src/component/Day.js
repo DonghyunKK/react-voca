3ce63b9d7cafbef9463e88bdb07c239221403e11
donghyun kim <donghyunkim524@gmail.com>
2022-01-04 20:40:39 +0000

    add prev / next link to day component

--- src/component/Day.js
+++ src/component/Day.js
@@ -7,21 +7,29 @@ function Day() {
   const { day } = useParams();
   // const wordList = dummy.words.filter(word => word.day === Number(day))
   // const [words, setWords] = useState([]);
-
+  
   // useEffect(() => {
-  //   fetch(`http://localhost:3001/words?day=${day}`)
-  //   .then(res => {
-  //     return res.json()
-  //   })
-  //   .then(data => {
-  //     setWords(data)
-  //   })
-  // }, [day])
+    //   fetch(`http://localhost:3001/words?day=${day}`)
+    //   .then(res =>   {
+      //     return res.json()
+      //   })
+      //   .then(data => {
+        //     setWords(data)
+        //   })
+        // }, [day])
+  
   const words = useFetch(`http://localhost:3001/words?day=${day}`)
+  const days = useFetch("http://localhost:3001/days")
 
   return (
     <>
-    <h2>Day {day}</h2>
+    <div className='dayContainer'>
+      <h2>Day {day}</h2>
+      <div className='links'>
+        {Number(day) !== 1 && <Link to={`/day/${Number(day)-1}`} className="link">Prev</Link>}
+        {days.length === Number(day) || <Link to={`/day/${Number(day)+1}`} className="link">Next</Link>}
+      </div>
+    </div>
     {words.length === 0 && <span>Loading...</span>}
       <table>
         <tbody>
